c7ee277a815f9a447646732dc67dddbe7119c811
MIRELLA DA SILVA SENE <25162437@edu119.local>
2025-11-14 13:34:04 -0300

    adicionando favoritos e reservas

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import usuariosRoutes from './routes/usuarios.routes.js';
-import livrosRoutes from './routes/livros.routes.js';
-import avaliacoesRoutes from './routes/avaliacoes.routes.js';
+import express from "express";
+import cors from "cors";
+import reservasRoutes from "./routes/reservas.routes.js";
+import favoritosRoutes from "./routes/favoritos.routes.js";
 
 const app = express();
 const PORT = 3000;
@@ -10,13 +9,8 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/usuarios', usuariosRoutes);
-app.use('/livros', livrosRoutes);
-app.use('/avaliacoes', avaliacoesRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Servidor da Livraria está funcionando!');
-});
+app.use("/reservas",reservasRoutes);
+app.use("/favoritos",favoritosRoutes);
 
 app.listen(PORT, () => {
   console.log(`✅ Servidor rodando em http://localhost:${PORT}`);
